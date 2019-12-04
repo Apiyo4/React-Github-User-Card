65bd9e06107b51855dda29284474b0fae150384f
Annette Odingo <odingoannette@gmail.com>
2019-12-04 05:25:53 +0000

    fetch data from api and display in the user component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
 import User from './components/User';
-import axios from 'axios';
+import Axios from 'axios';
 
 export default class App extends React.Component {
-  
+  constructor(){
+    super();
+    this.state = {
+      user : [],
+    }
+  }
+  componentDidMount(){
+    Axios.get('https://api.github.com/users/apiyo4?access_token=ca8a7abbd7a3d7b6c089d22b78056fd9e335919b')
+    .then(res=>{
+     
+      this.setState({
+        user: res.data,
+      })
+      console.log(this.state.user);
+    })
+  }
   render(){
     return (
       <div className="App">
         Hello world
-        <User />
+        <User  user= {this.state.user}/>
       </div>
     );
   }
